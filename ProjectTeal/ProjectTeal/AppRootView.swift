126b6faa8cbb7c521bfa90e53db7e8cb9cad2c8e
Ysy <shengyang998@gmail.com>
2025-12-17 19:40:58 +0800

    Inject shared app environment at entry point

--- ProjectTeal/ProjectTeal/AppRootView.swift
+++ ProjectTeal/ProjectTeal/AppRootView.swift
@@ -9,14 +9,26 @@ import SwiftUI
 import UIKit
 
 struct AppRootView: View {
+    let environment: AppEnvironment
+
+    init(environment: AppEnvironment = .shared) {
+        self.environment = environment
+    }
+
     var body: some View {
-        AppRootViewControllerRepresentable()
+        AppRootViewControllerRepresentable(environment: environment)
     }
 }
 
 struct AppRootViewControllerRepresentable: UIViewControllerRepresentable {
+    let environment: AppEnvironment
+
+    init(environment: AppEnvironment = .shared) {
+        self.environment = environment
+    }
+
     func makeUIViewController(context: Context) -> AppRootViewController {
-        return AppRootViewController()
+        return AppRootViewController(environment: environment)
     }
     
     func updateUIViewController(_ uiViewController: AppRootViewController, context: Context) {
--- ProjectTeal/ProjectTeal/ProjectTealApp.swift
+++ ProjectTeal/ProjectTeal/ProjectTealApp.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct ProjectTealApp: App {
+    private let environment = AppEnvironment.shared
+
     var body: some Scene {
         WindowGroup {
-            AppRootView()
+            AppRootView(environment: environment)
         }
     }
 }
